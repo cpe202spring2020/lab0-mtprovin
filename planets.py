55b4fb288d1b26a6d22da39c8de0ca838715657c
Matthew Province <mtprovin@unix1.csc.calpoly.edu>
2020-04-05 22:37:59 -0700

    got rid of space in print

--- planets.py
+++ planets.py
@@ -2,8 +2,7 @@ def weight_on_planets():
     weight = int(input("What do you weigh on earth? "))
     mars_weight = weight * .38
     jupiter_weight = weight * 2.34
-    print(f"\nOn Mars you would weigh {mars_weight:.2f} pounds.",
-f"\nOn Jupiter you would weigh {jupiter_weight:.2f} pounds.")
+    print(f"\nOn Mars you would weigh {mars_weight:.2f} pounds.\nOn Jupiter you would weigh {jupiter_weight:.2f} pounds.")
 
 
 if __name__ == '__main__':
